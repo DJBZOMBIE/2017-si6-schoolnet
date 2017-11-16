b5673d564dcd707ff7cc4483669838d87aab1d9b
DJBZOMBIE <djbzombie@hotmail.com>
2017-11-16 21:46:44 -0200

    comboBox

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = 3000
 
-app.get('/', (request, response) => response.send('Hi There'))
+//express setup
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public'))
+
+//templates
+app.set('views', path.join(__dirname,'public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (request, response) => {
+    const data = {
+      students: require('./db/studentsDB.json')
+    }
+    response.render('index', data)
+})
 
 app.listen(PORT, () => console.log('Server is running on port ' + PORT))
