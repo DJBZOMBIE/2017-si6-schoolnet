777c2913c4e2b07df52bf18c95c7ba7ede366844
DJBZOMBIE <djbzombie@hotmail.com>
2017-11-20 21:44:27 -0200

    test2

--- server.js
+++ server.js
@@ -9,14 +9,26 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 //templates
-app.set('views', path.join(__dirname,'public'))
+app.set('views', path.join(__dirname, 'public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (request, response) => {
-    const data = {
-      students: require('./db/studentsDB.json')
-    }
+    const data = { student:null, submitted:false }
     response.render('index', data)
 })
 
-app.listen(PORT, () => console.log('Server is running on port ' + PORT))
+app.get('/status', (request, response) => {
+    const { getStudentStatus } = require('./app/check-students')
+    studentStatus = getStudentStatus(request.query.studentId)
+    
+    const data = {
+        student: studentStatus,
+        submitted: true
+    }
+
+    response.render('index', data)
+})
+
+app.listen(PORT, () => { 
+    console.log('Server is running on port ' + PORT)
+})
